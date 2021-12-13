77926cfeec1689eeece7eec939b218efe94044ad
MrDogukan <aliseslikaya@gmail.com>
2021-12-13 21:33:29 +0300

    link commit

--- src/components/styled-comp/StyledComponents.js.js
+++ src/components/styled-comp/StyledComponents.js.js
@@ -2,12 +2,24 @@ import React from "react";
 import { Button } from "./button/Button.styles";
 import HeaderText from "./header/HeaderText";
 import Wrapper from "./wrapper/Wrapper";
+import Link from "./link/Link";
+import ExtendedComp from "./extended-link/ExtendedComp";
 
 const StyledComponents = () => {
   return (
     <Wrapper bgColor="yellow">
       <HeaderText>Clarusway</HeaderText>
       <HeaderText headerColor="red">Styled Components</HeaderText>
+      <Link
+        target="_blank"
+        rel="noopener"
+        href="https://www.google.com/"
+        secondary
+      >
+        {" "}
+        Learn FS Development
+      </Link>
+      <ExtendedComp secondary>Extended Link</ExtendedComp>
       <Button primary>Primary</Button>
       <Button>Primary</Button>
     </Wrapper>
